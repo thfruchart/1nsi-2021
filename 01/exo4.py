9fb47741b4b8fc34eea0a8749810aafe06f18e5b
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-09-17 14:42:30 +0200

    Add files via upload

--- 01/exo4.py
+++ 01/exo4.py
@@ -5,7 +5,9 @@ var1 = input("Entrer le texte var1 : ")
 var2 = input("Entrer le texte var2 : ")
 
 # échange du contenu de var1 et var2
-
+aux = var1
+var1 = var2
+var2 = aux
 
 # Affichage
 print("var1 : ", var1)
