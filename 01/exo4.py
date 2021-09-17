738d3cd4bf966f54292a82a2a388bc90aee261da
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-09-17 14:38:25 +0200

    Add files via upload

---
+++ 01/exo4.py
@@ -0,0 +1,12 @@
+# 01-EXERCICE4
+
+# Saisie des variables
+var1 = input("Entrer le texte var1 : ")
+var2 = input("Entrer le texte var2 : ")
+
+# échange du contenu de var1 et var2
+
+
+# Affichage
+print("var1 : ", var1)
+print("var2", var2, sep=" : ")
