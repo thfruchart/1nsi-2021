26d36ead6fe7a5ee7fa51af3833abde35b8764c4
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-09-30 14:00:11 +0200

    Update 01-Exo6.py

--- 01/01-Exo6.py
+++ 01/01-Exo6.py
@@ -1,3 +1,7 @@
 ﻿# Exercice 6 (à compléter)
 n = int(input("Entrer le nombre d'oeufs :"))
-print("nombre de boites : ",  n//6)
+print("nombre de boites : ", 1+ (n-1)//6)
+
+#vérification
+for n in range(13):
+    print (n, " => ",1+ (n-1)//6)
