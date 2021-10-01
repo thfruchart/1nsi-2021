13080627038e0383d654b8cf188f3fbd90367714
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-10-01 17:16:26 +0200

    Create exo4.py

---
+++ 02/exo4.py
@@ -0,0 +1,7 @@
+# exercice 4
+a = 0
+for i in range(101) :  # pour i allant de 0 à 100 inclus
+    a = a+i  # on ajoute i dans l'accumulateur a
+
+# une fois la boucle for terminée, on affiche le résultat
+print(a)
