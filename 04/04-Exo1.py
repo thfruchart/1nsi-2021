54a063f5609fb82c2fa1a6a519d5ef3e8a5d2791
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-10-07 14:53:48 +0200

    Add files via upload

---
+++ 04/04-Exo1.py
@@ -0,0 +1,11 @@
+﻿from turtle import *
+def rectangle(long, larg) :
+    forward(long)
+    left(90)
+    forward(larg)
+    left(90)
+    forward(long)
+    left(90)
+    forward(larg)
+    left(90)
+
