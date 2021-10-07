cdeb26aa7cf494e3ba6a0c583e5016e107b85926
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-10-07 14:28:20 +0200

    Add files via upload

---
+++ 03/Chap3-EXO4.py
@@ -0,0 +1,19 @@
+﻿from turtle import *
+
+n = int(input('Entrer n :'))
+
+rayon = 200//n
+dist = 3 * rayon
+up()
+backward(dist * n//2)
+
+for i in range(n):
+    if i%2 ==0 :
+        dot(rayon,'red')
+    else :
+        dot(rayon,'green')
+    forward(dist)
+
+    
+
+
