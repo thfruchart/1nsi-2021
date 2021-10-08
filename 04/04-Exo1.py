9827fdf413e15888e37af684beca658124ea8eea
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-10-08 17:07:24 +0200

    Add files via upload

--- 04/04-Exo1.py
+++ 04/04-Exo1.py
@@ -1,4 +1,6 @@
-﻿from turtle import *
+# Créé par thfruchart, le 08/10/2021 en Python 3.7
+from turtle import *
+
 def rectangle(long, larg) :
     forward(long)
     left(90)
@@ -9,3 +11,11 @@ def rectangle(long, larg) :
     forward(larg)
     left(90)
 
+speed(0)
+
+def rosace(n):
+    for i in range(n):
+        rectangle(200,100)
+        left(360/n)
+
+rosace(36)
---
+++ 04/04-Exo2.py
@@ -0,0 +1,7 @@
+# Créé par thfruchart, le 08/10/2021 en Python 3.7
+
+def max2( x , y ):
+    if x>y :
+        return x
+    else :
+        return y
