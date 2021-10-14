ac3e55e3997371361432ca5dab37af788b140f05
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-10-14 14:17:58 +0200

    Add files via upload

--- 04/04-Exo3.py
+++ 04/04-Exo3.py
@@ -1,7 +1,21 @@
 ﻿# Chap4 EXO3
-
 def max2(x,y):
     if x>y:
         return x
     else :
         return y
+
+def max3(x,y,z):
+    if x >= y and x >= y :
+        return x
+    if y >= x and y >= z:
+        return y
+    if z >= x and z >= y :
+        return z
+
+def max3(x,y,z) :
+    m = max2(x,y)
+    M = max2(m, z)
+    return M
+
+
